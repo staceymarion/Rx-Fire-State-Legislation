68ee8fa6767f6eed011f8f3a14c644530817e1e5
JoshRiebe <joriebe@wisc.edu>
2021-04-29 21:41:55 -0500

    lots of stuff

---
+++ main4-27.js
@@ -0,0 +1,76 @@
+//insert code here!
+window.onload = setMap();
+
+//set up choropleth map
+function setMap(){
+
+    var width = 900,
+        height = 500;
+    
+    var map = d3.select("body")
+        .append("svg")
+        .attr("class", "map")
+        .attr("width", width)
+        .attr("height", height);
+
+    var projection = d3.geoAlbersUsa()
+        .scale(900);
+        
+    var path = d3.geoPath()
+        .projection(projection);
+    console.log("hihihii");
+    d3.json("data/usaStates.topojson", function(data) {
+        console.log(data);  
+        callback(data);
+    });
+
+    function callback(data){
+        var usa = data;
+
+        var americanStates = topojson.feature(usa, usa.objects.usaStates).features;
+        console.log(americanStates);
+        
+        
+ 
+
+
+        var states = map.selectAll(".states")
+            .data(americanStates)
+            .enter()
+            .append("path")
+            .attr("class", function(d){
+                return "states " + d.properties.name;
+            })
+            .attr("d", path);
+    };
+
+    $(function() {
+    var Accordion = function(el, multiple) {
+    this.el = el || {};
+    // more then one submenu open?
+    this.multiple = multiple || false;
+
+    var dropdownlink = this.el.find('.dropdownlink');
+    dropdownlink.on('click',
+                    { el: this.el, multiple: this.multiple },
+                    this.dropdown);
+    };
+
+    Accordion.prototype.dropdown = function(e) {
+    var $el = e.data.el,
+        $this = $(this),
+        //this is the ul.submenuItems
+        $next = $this.next();
+
+    $next.slideToggle();
+    $this.parent().toggleClass('open');
+
+    if(!e.data.multiple) {
+      //show only one menu at the same time
+      $el.find('.submenuItems').not($next).slideUp().parent().removeClass('open');
+    }
+    }
+
+    var accordion = new Accordion($('.accordion-menu'), false);
+    })
+};
